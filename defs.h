f855a069b6788a1abcc6d7c24935ea9437db45f3
pranav-kirsur <pranav.kirsur@gmail.com>
2019-11-02 22:40:11 +0530

    Added waitx

--- defs.h
+++ defs.h
@@ -186,5 +186,9 @@ void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
 
+
+//added syscalls
+int waitx(int* , int* );
+
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
--- proc.c
+++ proc.c
@@ -112,7 +112,7 @@ found:
   memset(p->context, 0, sizeof *p->context);
   p->context->eip = (uint)forkret;
 
-  //Add timestamps to process
+  //Add times to process
   p->ctime = ticks;
   p->rtime = 0;
   p->etime = 0;
@@ -319,6 +319,63 @@ wait(void)
     sleep(curproc, &ptable.lock);  //DOC: wait-sleep
   }
 }
+
+
+//waitx syscall
+int
+waitx(int* wtime, int* rtime)
+{
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+
+
+        //Update times
+        *wtime = p->etime - p->ctime - p->rtime;
+        *rtime = p->rtime;
+
+        //Clean up times
+        p->ctime = 0;
+        p->etime = 0;
+        p->rtime = 0;
+        
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+
+
 
 //PAGEBREAK: 42
 // Per-CPU process scheduler.
