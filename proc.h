ccce9c12119a1164d5772d344b14c0984a9d7335
pranav-kirsur <pranav.kirsur@gmail.com>
2019-11-03 17:35:19 +0530

    Added proc_stat struct

--- proc.h
+++ proc.h
@@ -70,3 +70,12 @@ struct proc
 //   original data and bss
 //   fixed-size stack
 //   expandable heap
+
+struct proc_stat
+{
+  int pid;           // PID of each process
+  float runtime;     // Total runtime
+  int num_run;       // number of time the process is executed
+  int current_queue; // current assigned queue
+  int ticks[5];      // number of ticks each process has received at each of the 5 priority queues
+};
